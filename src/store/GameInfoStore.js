5e4f1fd15eddaca16102c4e50cf43a62de0e44c5
Jiho Park <wlgh325@naver.com>
2023-11-22 22:30:01 +0900

    Update GameInfoStore.js

--- src/store/GameInfoStore.js
+++ src/store/GameInfoStore.js
@@ -1,20 +1,32 @@
-import { makeObservable, observable } from "mobx";
+import {action, makeObservable, observable} from "mobx";
 import RootStore from "./RootStore";
 
-class GameInfoStore extends RootStore{
+class ScoreStore extends RootStore {
     score = 0;
+    bestScore = 0;
+    rootStore;
 
     constructor(rootStore) {
         super();
         this.rootStore = rootStore;
         makeObservable(this, {
             score: observable,
+            bestScore: observable,
+            addScore: action,
+            initScore: action,
+            updateBestScore: action
         })
     }
-    
+
     addScore = (point) => this.score = this.score + point;
 
     initScore = () => this.score = 0;
+
+    updateBestScore = () => {
+        if (this.bestScore < this.score) {
+            this.bestScore = this.score;
+        }
+    }
 }
 
-export default GameInfoStore;
+export default ScoreStore;
