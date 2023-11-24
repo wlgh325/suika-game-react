c48c1948f939d32bd6d53900febf5a9038dc1eb5
park.jiho <park.jiho@hyundai-autoever.com>
2023-11-24 15:08:01 +0900

    Separate Game component

---
+++ src/components/Game.js
@@ -0,0 +1,193 @@
+import {Body, Bodies, Engine, Events, Render, Runner, World} from "matter-js"
+import {observer} from "mobx-react-lite"
+import {useEffect, useRef, useMemo} from "react"
+import {useStore} from "../store";
+
+const Game = () => {
+    const gameInfoStore = useStore("gameInfoStore");
+    const {addScore, initScore, updateBestScore} = useStore("scoreStore")
+    const {theme, gravityY, gameCount, increaseGameCount} = gameInfoStore;
+    const canvasRef = useRef(null);
+
+    const gameOver = () => {
+        alert("game Over");
+
+        increaseGameCount();
+        updateBestScore();
+        initScore()
+    }
+
+    const fruits = useMemo(() => gameInfoStore.fruits, [theme]);
+    useEffect(() => {
+        const engine = Engine.create();
+        const render = Render.create({
+        engine,
+        canvas: canvasRef.current,
+        element: document.body,
+        options: {
+            wireframes: false,
+            background: "#F7F4C8",
+            width: 570,
+            height: 800,
+        }
+        });
+
+        engine.gravity.y = gravityY;
+        const world = engine.world;
+
+        const leftWall = Bodies.rectangle(15, 370, 30, 740, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+        });
+
+        const rightWall = Bodies.rectangle(555, 370, 30, 740, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+        });
+
+        const ground = Bodies.rectangle(285, 770, 570, 60, {
+        isStatic: true,
+        render: { fillStyle: "#E6B143" }
+        });
+
+        const topLine = Bodies.rectangle(285, 150, 570, 2, {
+        name: "topLine",
+        isStatic: true,
+        isSensor: true,
+        render: { fillStyle: "#E6B143" }
+        })
+
+        World.add(world, [leftWall, rightWall, ground, topLine]);
+
+        Render.run(render);
+        Runner.run(engine);
+
+        let currentBody = null;
+        let currentFruit = null;
+        let disableAction = false;
+        let interval = null;
+
+        function addFruit() {
+        const index = Math.floor(Math.random() * 5);
+        const fruit = fruits[index];
+
+        const body = Bodies.circle(300, 50, fruit.radius, {
+            index: index,
+            isSleeping: true,
+            render: {
+            sprite: { texture: `${fruit.name}.png` }
+            },
+            restitution: 0.2,
+        });
+
+        currentBody = body;
+        currentFruit = fruit;
+
+        World.add(world, body);
+        }
+
+        window.onkeydown = (event) => {
+        if (disableAction) {
+            return;
+        }
+
+        switch (event.code) {
+            case "KeyA":
+            if (interval)
+                return;
+
+            interval = setInterval(() => {
+                if (currentBody.position.x - currentFruit.radius > 30)
+                Body.setPosition(currentBody, {
+                    x: currentBody.position.x - gameInfoStore.distance,
+                    y: currentBody.position.y,
+                });
+            }, 5);
+            break;
+
+            case "KeyD":
+            if (interval)
+                return;
+
+            interval = setInterval(() => {
+                if (currentBody.position.x + currentFruit.radius < 540)
+                Body.setPosition(currentBody, {
+                x: currentBody.position.x + gameInfoStore.distance,
+                y: currentBody.position.y,
+                });
+            }, 5);
+            break;
+
+            case "KeyS":
+            currentBody.isSleeping = false;
+            disableAction = true;
+
+            setTimeout(() => {
+                addFruit();
+                disableAction = false;
+            }, 1000);
+            break;
+
+            default:
+            break;
+        }
+        }
+
+        window.onkeyup = (event) => {
+        switch (event.code) {
+            case "KeyA":
+            case "KeyD":
+            clearInterval(interval);
+            interval = null;
+            break;
+            default:
+            break;
+        }
+        }
+
+        Events.on(engine, "collisionStart", (event) => {
+        event.pairs.forEach((collision) => {
+            if (collision.bodyA.index === collision.bodyB.index) {
+            const index = collision.bodyA.index;
+
+            addScore(fruits[index].score * 2);
+
+            if (index === fruits.length - 1) {
+                return;
+            }
+
+            World.remove(world, [collision.bodyA, collision.bodyB]);
+
+            const newFruit = fruits[index + 1];
+
+            const newBody = Bodies.circle(
+                collision.collision.supports[0].x,
+                collision.collision.supports[0].y,
+                newFruit.radius,
+                {
+                render: {
+                    sprite: { texture: `${newFruit.name}.png` }
+                },
+                index: index + 1,
+                }
+            );
+
+            World.add(world, newBody);
+            }
+
+            if (!disableAction &&
+            (collision.bodyA.name === "topLine" || collision.bodyB.name === "topLine")) {
+            gameOver()
+            }
+        });
+        });
+
+        addFruit();
+    }, [gameCount, fruits])
+
+    return (
+        <canvas ref={canvasRef}/>
+    )
+}
+
+export default observer(Game);
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Score } from "./Score";
 export { default as Control } from "./Control";
+export { default as Game } from "./Game";
